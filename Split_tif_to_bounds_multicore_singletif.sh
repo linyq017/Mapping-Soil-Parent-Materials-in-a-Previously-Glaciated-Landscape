bc90b781f3d21cfa03fd25c58fde178bdf8a6812
linyq017 <106093623+linyq017@users.noreply.github.com>
2023-08-21 16:49:14 +0200

    Create Split_tif_to_bounds_multicore_singletif.sh

---
+++ Split_tif_to_bounds_multicore_singletif.sh
@@ -0,0 +1,18 @@
+
+cd /workspace/data/SGU/orginal_fran_wl/rastertindex
+
+# Define the number of parallel processes
+num_processes=100
+
+# Use ls and xargs for parallel processing of shapefiles
+ls *.shp | xargs -n 2 -P "$num_processes" bash -c $'
+    shapefile=$1
+    shapefile_name=$(basename "$shapefile" .shp)
+    output_tif="/workspace/Lidardata01/Lin/Data/Indices/tiles/CVA50/${shapefile_name}.tif"
+    tif="/workspace/Lidardata01/Lin/Data/Indices/resampled2m/CVA50.tif"
+    
+    gdalwarp -cutline "$shapefile" -crop_to_cutline "$tif" "$output_tif"
+    
+    echo "Cropped TIFF saved: $output_tif"
+    echo
+' _
