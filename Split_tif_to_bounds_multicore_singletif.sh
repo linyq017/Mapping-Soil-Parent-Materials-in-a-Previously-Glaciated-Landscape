370d9b6b62eb8735b84efbb50dd48192d6a3cf7c
linyq017 <106093623+linyq017@users.noreply.github.com>
2023-08-21 16:51:29 +0200

    Update Split_tif_to_bounds_multicore_singletif.sh
    changed -n from 2 to 1

--- Split_tif_to_bounds_multicore_singletif.sh
+++ Split_tif_to_bounds_multicore_singletif.sh
@@ -5,7 +5,7 @@ cd /workspace/data/SGU/orginal_fran_wl/rastertindex
 num_processes=100
 
 # Use ls and xargs for parallel processing of shapefiles
-ls *.shp | xargs -n 2 -P "$num_processes" bash -c $'
+ls *.shp | xargs -n 1 -P "$num_processes" bash -c $'  # -n is number of arguments
     shapefile=$1
     shapefile_name=$(basename "$shapefile" .shp)
     output_tif="/workspace/Lidardata01/Lin/Data/Indices/tiles/CVA50/${shapefile_name}.tif"
